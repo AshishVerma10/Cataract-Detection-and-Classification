cd261829d3d7cf80fc88f2c533895cb086e92b09
Ashish <vashishverma10@gmail.com>
2026-06-03 11:47:59 +0530

    Make app deployment ready

--- GUI/app.py
+++ GUI/app.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import pickle
 import cv2
@@ -6,65 +5,109 @@ from skimage.feature import graycomatrix, graycoprops
 import numpy as np
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, flash, request, redirect, url_for
-import time
-import h5py
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
-app.debug = True
+app.secret_key = "cataract_detection_secret"
 
-# Load models
-log_pickle_model = pickle.load(open("log_model.sav", 'rb'))
+# ---------------- PATH CONFIG ---------------- #
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# ---------------- MODEL LOADING ---------------- #
+
+log_pickle_model = pickle.load(
+    open(os.path.join(BASE_DIR, "log_model.sav"), "rb")
+)
+
 sift = cv2.SIFT_create()
 size = 128
-intensity_model = load_model('squeezenet.h5')
 
-# Upload folder config
-UPLOAD_FOLDER = './static/inputimages'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+intensity_model = load_model(
+    os.path.join(BASE_DIR, "squeezenet.h5")
+)
+
+# ---------------- UPLOAD FOLDER ---------------- #
+
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "inputimages")
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # ---------------- ROUTES ---------------- #
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template("home.html")
+
 
 @app.route("/about/")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
+
 
 @app.route("/main/")
 def main():
-    return render_template('main.html')
+    return render_template("main.html")
 
-@app.route('/upload_file/', methods=['POST'])
+
+@app.route("/upload_file/", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
 
-    file = request.files['file']
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(url_for("main"))
 
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
+    file = request.files["file"]
+
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(url_for("main"))
 
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-    return render_template('main.html', user_image=filename)
+    file_path = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        filename
+    )
 
-@app.route('/prediction/<file>', methods=['GET', 'POST'])
+    file.save(file_path)
+
+    return render_template(
+        "main.html",
+        user_image=filename
+    )
+
+
+@app.route("/prediction/<file>", methods=["GET", "POST"])
 def prediction(file):
+
     result1 = ""
     result2 = ""
 
-    image_path = "./static/inputimages/" + file
+    image_path = os.path.join(
+        app.config["UPLOAD_FOLDER"],
+        file
+    )
+
     image = cv2.imread(image_path, 0)
 
-    image_test = cv2.resize(image, (size, size), interpolation=cv2.INTER_AREA)
+    if image is None:
+        return redirect(
+            url_for(
+                "results",
+                result1="Invalid Image",
+                result2="Unable to process image"
+            )
+        )
+
+    image_test = cv2.resize(
+        image,
+        (size, size),
+        interpolation=cv2.INTER_AREA
+    )
 
     glcm_test = []
     images_sift_test = []
@@ -72,89 +115,257 @@ def prediction(file):
     img_arr_test = np.array(image_test)
 
     # GLCM Features
-    gCoMat = graycomatrix(img_arr_test, [1], [0], 256, symmetric=True, normed=True)
 
-    contrast = graycoprops(gCoMat, prop='contrast')[0][0]
-    dissimilarity = graycoprops(gCoMat, prop='dissimilarity')[0][0]
-    homogeneity = graycoprops(gCoMat, prop='homogeneity')[0][0]
-    energy = graycoprops(gCoMat, prop='energy')[0][0]
-    correlation = graycoprops(gCoMat, prop='correlation')[0][0]
+    gCoMat = graycomatrix(
+        img_arr_test,
+        [1],
+        [0],
+        256,
+        symmetric=True,
+        normed=True
+    )
+
+    contrast = graycoprops(
+        gCoMat,
+        prop="contrast"
+    )[0][0]
+
+    dissimilarity = graycoprops(
+        gCoMat,
+        prop="dissimilarity"
+    )[0][0]
+
+    homogeneity = graycoprops(
+        gCoMat,
+        prop="homogeneity"
+    )[0][0]
+
+    energy = graycoprops(
+        gCoMat,
+        prop="energy"
+    )[0][0]
+
+    correlation = graycoprops(
+        gCoMat,
+        prop="correlation"
+    )[0][0]
 
     # SIFT Features
-    keypoints, descriptors = sift.detectAndCompute(image_test, None)
+
+    keypoints, descriptors = sift.detectAndCompute(
+        image_test,
+        None
+    )
 
     if descriptors is None:
         descriptors = np.zeros((1, 2304))
 
     descriptors = descriptors.flatten()
 
-    glcm_test.append([contrast, dissimilarity, homogeneity, energy, correlation])
+    glcm_test.append([
+        contrast,
+        dissimilarity,
+        homogeneity,
+        energy,
+        correlation
+    ])
+
     glcm_test = np.array(glcm_test)
 
-    images_sift_test.append(descriptors[:2304])
-    images_sift_test = np.array(images_sift_test)
+    images_sift_test.append(
+        descriptors[:2304]
+    )
 
-    # Combine features
-    features = np.concatenate((images_sift_test, glcm_test), axis=1)
+    images_sift_test = np.array(
+        images_sift_test
+    )
 
-    # Prediction
-    if log_pickle_model.predict(features) == 1:
-        result1 = "Cataract detected"
+    # Combine Features
+
+    features = np.concatenate(
+        (
+            images_sift_test,
+            glcm_test
+        ),
+        axis=1
+    )
+
+    # Binary Classification
+
+    prediction_result = log_pickle_model.predict(features)
+
+    if prediction_result == 1:
+
+        result1 = "Cataract Detected"
 
         img = cv2.imread(image_path)
-        img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
 
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        img = cv2.resize(
+            img,
+            (224, 224),
+            interpolation=cv2.INTER_AREA
+        )
+
+        hsv = cv2.cvtColor(
+            img,
+            cv2.COLOR_BGR2HSV
+        )
 
         sensitivity = 156
-        lower_white = np.array([0, 0, 255 - sensitivity])
-        upper_white = np.array([255, sensitivity, 255])
 
-        mask = cv2.inRange(hsv, lower_white, upper_white)
+        lower_white = np.array(
+            [0, 0, 255 - sensitivity]
+        )
 
-        circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 1.5, 100000,
-                                   param1=80, param2=40, minRadius=0, maxRadius=0)
+        upper_white = np.array(
+            [255, sensitivity, 255]
+        )
 
-        x, y, r = 0, 0, 0
+        mask = cv2.inRange(
+            hsv,
+            lower_white,
+            upper_white
+        )
+
+        circles = cv2.HoughCircles(
+            mask,
+            cv2.HOUGH_GRADIENT,
+            1.5,
+            100000,
+            param1=80,
+            param2=40,
+            minRadius=0,
+            maxRadius=0
+        )
+
+        x, y, r = 112, 112, 100
+
         if circles is not None:
-            circles = np.uint16(np.around(circles))
+            circles = np.uint16(
+                np.around(circles)
+            )
+
             x, y, r = circles[0][0]
 
-        mask = np.zeros((224, 224), np.uint8)
-        cv2.circle(mask, (int(x), int(y)), int(r), (255, 255, 255), -1)
+        mask = np.zeros(
+            (224, 224),
+            np.uint8
+        )
 
-        masked_data = cv2.bitwise_and(img, img, mask=mask)
+        cv2.circle(
+            mask,
+            (int(x), int(y)),
+            int(r),
+            (255, 255, 255),
+            -1
+        )
 
-        _, thresh = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
-        cnt = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        masked_data = cv2.bitwise_and(
+            img,
+            img,
+            mask=mask
+        )
 
-        x, y, w, h = cv2.boundingRect(cnt[0])
+        _, thresh = cv2.threshold(
+            mask,
+            1,
+            255,
+            cv2.THRESH_BINARY
+        )
 
-        crop = masked_data[y:y + h, x:x + w]
-        crop = cv2.resize(crop, (224, 224), interpolation=cv2.INTER_AREA)
+        contours = cv2.findContours(
+            thresh,
+            cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE
+        )[0]
 
-        crop = cv2.cvtColor(crop, cv2.COLOR_BGR2RGB)
+        if len(contours) > 0:
+
+            x, y, w, h = cv2.boundingRect(
+                contours[0]
+            )
+
+            crop = masked_data[
+                y:y + h,
+                x:x + w
+            ]
+
+        else:
+
+            crop = masked_data
+
+        crop = cv2.resize(
+            crop,
+            (224, 224),
+            interpolation=cv2.INTER_AREA
+        )
+
+        crop = cv2.cvtColor(
+            crop,
+            cv2.COLOR_BGR2RGB
+        )
 
         my_image = img_to_array(crop)
-        my_image = my_image.reshape((1, 224, 224, 3))
 
-        ans = intensity_model.predict(my_image)
+        my_image = my_image.reshape(
+            (1, 224, 224, 3)
+        )
+
+        ans = intensity_model.predict(
+            my_image,
+            verbose=0
+        )
+
         ans_class = np.argmax(ans)
 
-        classes = ["Mild Cataract", "Normal Cataract", "Severe Cataract"]
+        classes = [
+            "Mild Cataract",
+            "Normal Cataract",
+            "Severe Cataract"
+        ]
+
         result2 = classes[ans_class]
 
     else:
+
         result1 = "No Cataract"
         result2 = "Normal Eye"
 
-    return redirect(url_for('results', result1=result1, result2=result2))
+    return redirect(
+        url_for(
+            "results",
+            result1=result1,
+            result2=result2
+        )
+    )
 
-@app.route('/result/<result1>/<result2>', methods=['GET', 'POST'])
+
+@app.route(
+    "/result/<result1>/<result2>",
+    methods=["GET", "POST"]
+)
 def results(result1, result2):
-    return render_template('result.html', result1=result1, result2=result2)
+
+    return render_template(
+        "result.html",
+        result1=result1,
+        result2=result2
+    )
+
 
 # ---------------- RUN ---------------- #
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
